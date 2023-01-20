1dac87afdc8cfe34b665a154577d6ce185f9d85d
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-20 16:10:24 +0330

    adding necessary references

---
+++ admin/admin_login.php
@@ -0,0 +1,20 @@
+<?php
+
+include '../components/connect.php';
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- icons cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <!-- our custom css -->
+    <link rel="stylesheet" href="../css/admin_style.css">
+</head>
+<body>
+    
+</body>
+</html>
