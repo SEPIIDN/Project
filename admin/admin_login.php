b8611b04349150668eaa6dc3855fd5d7e7964244
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-20 19:01:28 +0330

    styling and adding the admin recognition function

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -2,6 +2,27 @@
 
 include '../components/connect.php';
 
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+
+   $select_admin = $dbh->prepare("SELECT * FROM `admins` WHERE name = ? AND password = ?");
+   $select_admin->execute([$name, $pass]);
+   $row = $select_admin->fetch(PDO::FETCH_ASSOC);
+
+   if($select_admin->rowCount() > 0){
+      $_SESSION['admin_id'] = $row['id'];
+      header('location:dashboard.php');
+   }else{
+      $message[] = 'incorrect username or password!';
+   }
+
+}
 ?> 
 <html lang="en">
 <head>
