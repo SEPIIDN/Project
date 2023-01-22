cf52fb8fb27e6a6f8a6fcf0c5007b7cc7c8418f7
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-22 14:52:46 +0330

    added functionality to the delete button

--- admin/admin_accounts.php
+++ admin/admin_accounts.php
@@ -10,6 +10,14 @@ if (!isset($admin_id)) {
     header('location:admin_login.php');
 }
 
+if (isset($_GET['delete'])) {
+    $delete_id = $_GET['delete'];
+    $delete_admins = $conn->prepare("DELETE FROM `admins` WHERE id = ?");
+    $delete_admins->execute([$delete_id]);
+    header('location:admin_accounts.php');
+}
+
+?>
 ?>
 
 <html lang="en">
