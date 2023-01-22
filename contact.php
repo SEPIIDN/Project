1d9ea6697729df77f46eec19532d610c17d7c803
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-22 20:27:30 +0330

    finished the raw UI of the contacts section

---
+++ contact.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>contact</title>
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="contact">
+
+   <form action="" method="post">
+      <h3>get in touch</h3>
+      <input type="text" name="name" placeholder="enter your name" required maxlength="20" class="box">
+      <input type="email" name="email" placeholder="enter your email" required maxlength="50" class="box">
+      <input type="number" name="number" min="0" max="9999999999" placeholder="enter your number" required onkeypress="if(this.value.length == 10) return false;" class="box">
+      <textarea name="msg" class="box" placeholder="enter your message" cols="30" rows="10"></textarea>
+      <input type="submit" value="send message" name="send" class="btn">
+   </form>
+
+</section>
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
