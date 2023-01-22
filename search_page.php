aa47a6daa6bf19ae943e24ed5ac384494745c823
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-22 21:28:31 +0330

    connected the search to the db

--- search_page.php
+++ search_page.php
@@ -1,3 +1,18 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+include 'components/wishlist_cart.php';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
