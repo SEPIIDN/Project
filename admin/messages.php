f672940eca84826a823e56b21be3342e4b309240
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-22 15:53:55 +0330

    added the basic UI for the messages section

--- admin/messages.php
+++ admin/messages.php
@@ -6,7 +6,7 @@ session_start();
 
 $admin_id = $_SESSION['admin_id'];
 
-if(isset($admin_id)){
+if(!isset($admin_id)){
    header('location:admin_login.php');
 }
 
@@ -24,7 +24,38 @@ if(isset($admin_id)){
     <link rel="stylesheet" href="../css/admin_style.css">
 </head>
 <body>
+<?php include '../components/admin_header.php'; ?>
 
+<section class="contacts">
+
+<h1 class="heading">messages</h1>
+
+<div class="box-container">
+
+   <?php
+      $select_messages = $conn->prepare("SELECT * FROM `messages`");
+      $select_messages->execute();
+      if($select_messages->rowCount() > 0){
+         while($fetch_message = $select_messages->fetch(PDO::FETCH_ASSOC)){
+   ?>
+   <div class="box">
+   <p> user id : <span><?= $fetch_message['user_id']; ?></span></p>
+   <p> name : <span><?= $fetch_message['name']; ?></span></p>
+   <p> email : <span><?= $fetch_message['email']; ?></span></p>
+   <p> number : <span><?= $fetch_message['number']; ?></span></p>
+   <p> message : <span><?= $fetch_message['message']; ?></span></p>
+   <a href="messages.php??delete=<?= $fetch_message['id']; ?>" onclick="return confirm('delete this message?');" class="delete-btn">delete</a>
+   </div>
+   <?php
+         }
+      }else{
+         echo '<p class="empty">you have no messages</p>';
+      }
+   ?>
+
+</div>
+
+</section>
     <!-- admin_scripts -->
     <script src="../js/admin_script.js"></script>
 </body>
