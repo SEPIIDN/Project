2cc96b532ca8947958c07178d7d3bc37203fc82b
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-22 16:22:53 +0330

    added delete message function

--- admin/messages.php
+++ admin/messages.php
@@ -10,6 +10,12 @@ if(!isset($admin_id)){
    header('location:admin_login.php');
 }
 
+if(isset($_GET['delete'])){
+    $delete_id = $_GET['delete'];
+    $delete_message = $conn->prepare("DELETE FROM `messages` WHERE id = ?");
+    $delete_message->execute([$delete_id]);
+    header('location:messages.php');
+ }
 ?>
 
 <html lang="en">
