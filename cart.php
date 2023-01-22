a06cb39ea7d53c234ab2ffe61191c553df24cc0f
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-22 19:33:34 +0330

    added functionality to the cart section

--- cart.php
+++ cart.php
@@ -1,3 +1,39 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+   header('location:user_login.php');
+};
+
+if(isset($_POST['delete'])){
+   $cart_id = $_POST['cart_id'];
+   $delete_cart_item = $conn->prepare("DELETE FROM `cart` WHERE id = ?");
+   $delete_cart_item->execute([$cart_id]);
+}
+
+if(isset($_GET['delete_all'])){
+   $delete_cart_item = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
+   $delete_cart_item->execute([$user_id]);
+   header('location:cart.php');
+}
+
+if(isset($_POST['update_qty'])){
+   $cart_id = $_POST['cart_id'];
+   $qty = $_POST['qty'];
+   $qty = filter_var($qty, FILTER_SANITIZE_STRING);
+   $update_qty = $conn->prepare("UPDATE `cart` SET quantity = ? WHERE id = ?");
+   $update_qty->execute([$qty, $cart_id]);
+   $message[] = 'cart quantity updated';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
