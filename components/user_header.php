93adc355e7a22bb2c8c74ffbf2b06a0326f725cc
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-22 16:54:05 +0330

    making the header fancier!

--- components/user_header.php
+++ components/user_header.php
@@ -1,3 +1,16 @@
+<?php
+   if(isset($message)){
+      foreach($message as $message){
+         echo '
+         <div class="message">
+            <span>'.$message.'</span>
+            <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+         </div>
+         ';
+      }
+   }
+?>
+
 <header class="header">
 
    <section class="flex">
