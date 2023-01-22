9b07ff9b7decd2d5df48a6130fa6cdabed6b8e14
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-22 14:46:52 +0330

    added the ui for admin accounts section

--- admin/admin_accounts.php
+++ admin/admin_accounts.php
@@ -6,13 +6,14 @@ session_start();
 
 $admin_id = $_SESSION['admin_id'];
 
-if(isset($admin_id)){
-   header('location:admin_login.php');
+if (!isset($admin_id)) {
+    header('location:admin_login.php');
 }
 
 ?>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,9 +24,56 @@ if(isset($admin_id)){
 
     <link rel="stylesheet" href="../css/admin_style.css">
 </head>
+
 <body>
+
+    <section class="accounts">
+
+        <h1 class="heading">admin accounts</h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <p>add new admin</p>
+                <a href="register_admin.php" class="option-btn">register admin</a>
+            </div>
+
+            <?php
+            $select_accounts = $conn->prepare("SELECT * FROM `admins`");
+            $select_accounts->execute();
+            if ($select_accounts->rowCount() > 0) {
+                while ($fetch_accounts = $select_accounts->fetch(PDO::FETCH_ASSOC)) {
+                    ?>
+                    <div class="box">
+                        <p> admin id : <span>
+                                <?= $fetch_accounts['id']; ?>
+                            </span> </p>
+                        <p> admin name : <span>
+                                <?= $fetch_accounts['name']; ?>
+                            </span> </p>
+                        <div class="flex-btn">
+                            <a href="admin_accounts.php?delete=<?= $fetch_accounts['id']; ?>"
+                                onclick="return confirm('delete this account?')" class="delete-btn">delete</a>
+                            <?php
+                            if ($fetch_accounts['id'] == $admin_id) {
+                                echo '<a href="update_profile.php" class="option-btn">update</a>';
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <?php
+                }
+            } else {
+                echo '<p class="empty">no accounts available!</p>';
+            }
+            ?>
+
+        </div>
+
+    </section>
 
     <!-- admin_scripts -->
     <script src="../js/admin_script.js"></script>
 </body>
+
 </html>
